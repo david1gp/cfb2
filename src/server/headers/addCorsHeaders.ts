4b7a19c9d3de56d3f854495a1d0ef4d649c20e1e
David Siewert <david1gruppenplan@gmail.com>
2026-01-30 13:22:38 +0600

    refactor(headers): move into server dir

--- src/server/headers/addCorsHeaders.ts
+++ src/server/headers/addCorsHeaders.ts
@@ -1,5 +1,5 @@
-import { getCorsHeaders } from "@/headers/getCorsHeaders"
 import type { Env } from "@/env/Env"
+import { getCorsHeaders } from "@/server/headers/getCorsHeaders"
 
 export function addCorsHeaders(response: Response, env: Env, request: Request): Response {
   const corsHeaders = getCorsHeaders(env, request)
--- src/server/headers/getCorsHeaders.ts
+++ src/server/headers/getCorsHeaders.ts
@@ -20,7 +20,7 @@ export function getCorsHeaders(env: Env, request: Request): Headers {
   const maxAge = env.CORS_MAX_AGE || "300"
   headers.set("Access-Control-Max-Age", maxAge)
 
-  headers.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT")
+  headers.set("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST")
   headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization, If-Modified-Since")
 
   return headers
